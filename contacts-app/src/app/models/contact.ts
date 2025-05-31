4232c24b402833f2b896394135863278947c3bb2
Kravchenko Georgy <88142456+Kravchenko228@users.noreply.github.com>
2025-05-31 13:01:48 +0000

    Setting up + contact service

---
+++ contacts-app/src/app/models/contact.ts
@@ -0,0 +1,11 @@
+export interface Contact {
+    id?: number;
+    name: string;
+    fullAddress: string;
+    email: string;
+    phone: string;
+    cell: string;
+    registrationDate: string;
+    age: number;
+    imageUrl: string;
+}
---
+++ contacts-app/src/app/services/contacts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactsService {
+  apiUrl = "https://userscan.azurewebsites.net"
+  constructor(private http: HttpClient) { }
+  getContacts(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/contacts`);
+  }
+  getContact(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/api/contacts/${id}`);
+  }
+  addContact(contact: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/api/contacts`, contact);
+  }
+  updateContact(id: number, contact: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/api/contacts/${id}`, contact);
+  }
+  deleteContact(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/api/contacts/${id}`);
+  }
+
+}
