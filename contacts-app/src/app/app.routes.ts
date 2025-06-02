a8d33eb455fc991ae6a6c10e4a757c9507efc5e0
Kravchenko Georgy <88142456+Kravchenko228@users.noreply.github.com>
2025-06-02 17:08:05 +0000

    edit and delete implemented

--- contacts-app/src/app/app.routes.ts
+++ contacts-app/src/app/app.routes.ts
@@ -1,11 +1,19 @@
 import { Routes } from '@angular/router';
 import { ContactList } from './pages/contact-list/contact-list';
+import { ContactDetails } from './pages/contact-details/contact-details';
 
 export const routes: Routes = [
     {
         path: '',
         component: ContactList,
         title: 'Contact List',
+    }
+    ,
+    {
+        path: 'contact/:id',
+        component: ContactDetails,
+        title: 'Contact Details',
+
     }
 
 ];
--- contacts-app/src/app/pages/contact-list/contact-list.ts
+++ contacts-app/src/app/pages/contact-list/contact-list.ts
@@ -66,7 +66,7 @@ export class ContactList implements OnInit {
   }
 
   newContact(): void {
-    this.router.navigate(['/contact/new']);
+    this.router.navigate(['/contact', 'new']);
   }
 
 }
